b33f1b2746e7bb9203d6e72d8b3e7e2208e5801d
Moohammad Samaae <moohammad.samaae@bonnierpharmainsights.se>
2020-08-03 18:14:36 +0200

    Initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" alt="" class="w-100">
+        </div>
+        <div class="col-9">
+            {{-- Username --}}
+            <h1>LaraGram</h1>
+            {{-- Posts, followers, following --}}
+            <div class="d-flex">
+                <div class="mr-5"><strong>123</strong> posts</div>
+                <div class="mr-5"><strong>123</strong> followers</div>
+                <div class="mr-5"><strong>123</strong> following</div>
             </div>
+            {{-- Title --}}
+            {{-- Description --}}
+            {{-- URL --}}
         </div>
     </div>
 </div>
